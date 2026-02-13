70cc7f9b08d82c321339b1e82a3ffe794faf373a
Kshitij Londhe <kshitij.londhe01@gmail.com>
2026-02-13 13:55:08 +0530

    Added Get feature for User by ID and for User by Car model

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ const users = require("./MOCK_DATA.json");
 const app = express();
 const port = 8000;
 //Routes 
-app.get("/users",(req,res) => {
+app.get("/api/users",(req,res) => {
     return res.json(users);
+})
+app.get("/users", (req, res) => {
+    const html = `
+    <ul>
+        ${users.map(user => `<li>${user.first_name}</li>`).join('')}
+    </ul>
+    `;
+    res.send(html);
+});
+
+app.get("/api/users/:id", (req,res) =>{
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+});
+
+app.get("/api/users/car/:car_model", (req,res) => {
+    const car = (req.params.car_model);
+    const user = users.find((user) => user.car_model === car);
+    return res.json(user);
 })
 app.listen(port, () => console.log(`Server is running on port ${port}`));
