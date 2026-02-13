5a7c7e6f2bbdf02bbfe536083947dddc53e9b76c
Kshitij Londhe <kshitij.londhe01@gmail.com>
2026-02-13 15:22:52 +0530

    Grouped get, match, delete commands under one route

--- index.js
+++ index.js
@@ -15,15 +15,43 @@ app.get("/users", (req, res) => {
     res.send(html);
 });
 
-app.get("/api/users/:id", (req,res) =>{
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-});
+
 
 app.get("/api/users/car/:car_model", (req,res) => {
     const car = (req.params.car_model);
     const user = users.find((user) => user.car_model === car);
     return res.json(user);
 })
+
+app.post("/api/users", (req,res) => {
+    //To DO : Create a new usr
+    return res.json({ status : "pending" });
+});
+app
+
+//grouping of routes with same path
+.route("/api/users/:id")
+    .get((req,res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+})
+.patch((req,res) => {
+    //To DO : Edit a user with id
+    return res.json({ status : "pending" });
+})
+.delete((req,res) => {
+    //To DO : Delete the user with id
+    return res.json({ status : "pending" });
+});
+// app.patch("/api/users/id:", (req,res) => {
+//     //To DO : Edit a user with id
+//     return res.json({ status : "pending" });
+// });
+// app.delete("/api/users/id:", (req,res) => {
+//     //To DO : Delete the user with id
+//     return res.json({ status : "pending" });
+// });
+
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
